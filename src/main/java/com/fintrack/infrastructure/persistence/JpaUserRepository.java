f66dde125c70585844501422c50e8e8397a9b9f0
Sarthak <sarthakchaudhary@zohomail.in>
2026-03-09 09:00:12 +0530

    made basic folders for projects and added some domain files as well

---
+++ src/main/java/com/fintrack/infrastructure/persistence/JpaUserRepository.java
@@ -0,0 +1,14 @@
+package com.fintrack.infrastructure.persistence;
+
+import com.fintrack.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface JpaUserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
